f64afbf10ece8717914a39f2ceba16e9c4bcfa64
Matsuuu <huhta.matias@gmail.com>
2023-12-20 19:01:22 +0200

    Prevent ctrl s

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ if (!customElements.get("code-in-the-dim")) {
     customElements.define("code-in-the-dim", EditorElement);
 }
 
+
+window.addEventListener("keydown", ev => {
+    if (ev.key === "s" && ev.ctrlKey) {
+        ev.preventDefault();
+    }
+})
