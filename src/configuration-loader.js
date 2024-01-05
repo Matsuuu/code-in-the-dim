107fd6b4c4b740a5db4c680fd75e720b17064dca
Matsuuu <huhta.matias@gmail.com>
2024-01-05 17:34:35 +0200

    Support for multiple configurations

---
+++ src/configuration-loader.js
@@ -0,0 +1,18 @@
+function loadConfiguration() {
+    const configPath = getConfigPath();
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = configPath;
+
+    document.head.appendChild(script);
+}
+
+function getConfigPath() {
+    const urlParams = new URLSearchParams(window.location.href);
+    if (urlParams.has("config")) {
+        return "configurations/" + urlParams.get("config") + ".js";
+    }
+    return "configurations/default.js";
+}
+
+loadConfiguration();
